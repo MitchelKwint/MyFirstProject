49dfd84535d0f207650fa1f3ca5e61d2ce28f420
Gebruiker <Gebruiker@DESKTOP-VKLEM84>
2019-03-07 12:37:31 +0100

    Release 1.0.1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MyFirstProject</title>
+    </head>
+    <body>
+    <form action="" method="post">
+        <input type="text" name="name"><br>
+        <input type="submit" value="Start">
+    </form>
+        <?php
+        ?>
+    </body>
+</html>
